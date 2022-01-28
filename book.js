28caf32e37ef306f409d25ebe7579051cb2b26c7
zieeco <zieecosam@gmail.com>
2022-01-28 18:35:02 +0100

    Update all the files in the src folder

--- book.js
+++
@@ -1,8 +0,0 @@
-export default class Book {
-  constructor(title, author, pages, read = false) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-  }
-}
